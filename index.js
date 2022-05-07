9bccd5da2f0ece1988120dd80f67f02df9a6ffd4
Shahadat91 <shahadat20000@gmail.com>
2022-05-07 19:19:31 +0600

    mongo user created

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wd1z6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log('inventory management connected');
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+
+}
+
+run().catch(console.dir);
 
 
 
